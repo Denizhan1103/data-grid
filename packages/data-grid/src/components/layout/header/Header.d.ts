5cf6d3f77ee77abc51c0bca2f5c35fd4b69d90f7
Denizhan <denizhanonr2000@gmail.com>
2023-06-13 22:35:24 +0300

    feat:added header component to project

---
+++ packages/data-grid/src/components/layout/header/Header.d.ts
@@ -0,0 +1,9 @@
+export interface NavbarItem {
+  name: string;
+  to?: string;
+}
+
+export interface IconItem {
+  icon?: string;
+  to?: string;
+}
---
+++ packages/data-grid/src/components/layout/header/Header.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+import type { NavbarItem, IconItem } from "./Header.d";
+
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src="" />
+      <section className="header__navbar">
+        <HeaderNavbarList />
+        <HeaderIconList />
+      </section>
+    </header>
+  );
+}
+
+function HeaderNavbarList() {
+  // TODO: implement router
+  // const router = useRouter()
+
+  const navbarItems: NavbarItem[] = [
+    {
+      name: "Hakkımızda",
+      to: "#",
+    },
+    {
+      name: "Juri - Yarışma Yazılımı",
+      to: "#",
+    },
+    {
+      name: "Word Ninja",
+      to: "#",
+    },
+    {
+      name: "Word Pyramids",
+      to: "#",
+    },
+  ];
+
+  return (
+    <ul className="headernavbar">
+      {navbarItems.map(({ name, to }) => {
+        // Todo: implement router
+        return <li className="headernavbar__item">{name}</li>;
+      })}
+    </ul>
+  );
+}
+
+function HeaderIconList() {
+  const iconList: IconItem[] = [
+    {
+      icon: "",
+      to: "#",
+    },
+    {
+      icon: "",
+      to: "#",
+    },
+    {
+      icon: "",
+      to: "#",
+    },
+    {
+      icon: "",
+      to: "#",
+    },
+  ];
+
+  return (
+    <ul className="headericon">
+      {iconList.map(({ icon, to }) => {
+        return (
+          <li className="headericon__item">
+            <img src={icon} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
